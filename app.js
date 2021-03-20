facc83dc77a5d6fa0afb6d65cf05c6011c5a9473
leandrojcz <lenadro.casique@globalesm.com>
2021-03-20 10:59:55 -0400

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 
 const app = express();
 const db = require('./config/db');
@@ -6,6 +7,7 @@ const apiRouter = require('./routes/api');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api', apiRouter);
 
